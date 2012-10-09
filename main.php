ab8cbc6ede3765318e70164700383a094aaea7fa
MaxExite <max.dude@mail.ru>
2012-10-09 17:20:31 +0400

    tgfre

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: user
+ * Date: 09.10.12
+ * Time: 17:19
+ * To change this template use File | Settings | File Templates.
+ */
+$a=324;
+$b=12;
+echo $a+$b;
+?>
