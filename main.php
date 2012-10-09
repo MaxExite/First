a16190242dd46d41a933fa634e1006b1cb0c72c2
MaxExite <max.dude@mail.ru>
2012-10-09 17:19:06 +0400

    tgfre

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: user
+ * Date: 09.10.12
+ * Time: 17:16
+ * To change this template use File | Settings | File Templates.
+ */
+$time=date('H.i.s');
+echo 'Текущее время: ',$time;
+?>
